9b5adacb301ee0be129d86787682acc17c371d55
koke-desu <gbpvp25645@yahoo.co.jp>
2022-01-11 15:43:46 +0900

    tsでのdataの型を判断しやすくするため、返り値を変更

--- database/useCities.ts
+++ database/useCities.ts
@@ -1,5 +1,6 @@
 import React from "react";
 import useSWR from "swr";
+import { CityType } from "./dataType";
 import { citiesMockData } from "./mockData";
 
 // 市町村の一覧を獲得する関数。今はモックデータを返すだけ。
@@ -9,11 +10,5 @@ const fetcher = async () => {
 
 // 市町村の一覧を獲得するhooks。
 export const useCities = () => {
-  const { data, error } = useSWR("cities", fetcher);
-
-  return {
-    cities: data,
-    isLoading: !error && !data,
-    isError: error,
-  };
+  return useSWR("cities", fetcher);
 };
