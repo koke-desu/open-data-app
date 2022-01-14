6d4771bee6ba7ed5fe96b0620aa19b76940a6936
koke-desu <gbpvp25645@yahoo.co.jp>
2022-01-14 18:05:46 +0900

    CityCardのstorybookを作成

---
+++ components/model/city/CityCard.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import CityCard from "./CityCard";
+import { citiesMockData } from "../../../database/mockData";
+
+const mockCity = citiesMockData[0];
+
+export default { component: CityCard } as ComponentMeta<typeof CityCard>;
+
+export const Primary: ComponentStory<typeof CityCard> = () => <CityCard city={mockCity} />;
