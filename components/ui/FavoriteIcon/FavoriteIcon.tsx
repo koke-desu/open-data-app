81070fa728a0f0cdcce35f294df8e9ae5f8050f4
koke-desu <gbpvp25645@yahoo.co.jp>
2022-01-20 21:57:40 +0900

    onClickをpropsに追加

--- components/ui/FavoriteIcon/FavoriteIcon.tsx
+++ components/ui/FavoriteIcon/FavoriteIcon.tsx
@@ -4,17 +4,18 @@ import MUIFavoriteBorder from "@mui/icons-material/FavoriteBorder";
 
 type Props = {
   isFavorite: boolean;
+  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 };
 
-const FavoriteIcon: React.VFC<Props> = ({ isFavorite }) => {
+const FavoriteIcon: React.VFC<Props> = ({ isFavorite, onClick }) => {
   return (
-    <>
+    <div onClick={onClick}>
       {isFavorite ? (
         <MUIFavoriteIcon style={{ color: "#f88" }} sx={{ fontSize: 48 }} />
       ) : (
         <MUIFavoriteBorder style={{ color: "#999" }} sx={{ fontSize: 48 }} />
       )}
-    </>
+    </div>
   );
 };
 export default FavoriteIcon;
