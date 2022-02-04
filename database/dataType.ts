c611e2dfbf223fec01060a25a0eaa3c67003f56e
koke-desu <gbpvp25645@yahoo.co.jp>
2022-02-04 10:59:41 +0900

    mockデータを本番用データに置き換え、formatする関数を定義

--- database/dataType.ts
+++ database/dataType.ts
@@ -3,28 +3,58 @@
 // 各市町村のデータ
 export type CityType = {
   id: string;
-  prefecture: string;
-  name: string;
-  link: string;
-  crimeRate: number;
-  temperature: number;
-  population: number;
+  Pre: string;
+  Mun: string;
+  Pop: number;
+  Url: string;
+  Temp_Jan: number;
+  Temp_July: number;
+  Snow_Jan: number;
+  Bicthe_num: number;
+  Bicthe_rate: number;
+  Carthe_num: number;
+  Carthe_rate: number;
+  Carac_num: number;
+  Carac_rate: number;
+  Snathe_num: number;
+  Snathe_rate: number;
 };
+
+// モックデータやAPIから読み込まれるcityの型。
+export type importedCitiesType = { [key in keyof CityType]: string }[];
 
 // CityTypeのkeyの一覧。ループ回すのに使う。
 export const cityTypeKey: (keyof CityType)[] = [
   "id",
-  "prefecture",
-  "name",
-  "link",
-  "crimeRate",
-  "temperature",
-  "population",
+  "Pre",
+  "Mun",
+  "Pop",
+  "Url",
+  "Temp_Jan",
+  "Temp_July",
+  "Snow_Jan",
+  "Bicthe_num",
+  "Bicthe_rate",
+  "Carthe_num",
+  "Carthe_rate",
+  "Carac_num",
+  "Carac_rate",
+  "Snathe_num",
+  "Snathe_rate",
 ];
 
 // 表で表示するフィールドと、ラベルの対応を定義。
 export const cityStatusLabel: { [key in keyof Partial<CityType>]: string } = {
-  population: "人口（人）",
-  crimeRate: "犯罪率（％）",
-  temperature: "平均気温（℃）",
+  Pop: "人口（人）",
+  Temp_Jan: "1月の平均気温（℃）",
+  Temp_July: "7月の平均気温（℃）",
+  Snow_Jan: "1月の降雪量（cm）",
+  Bicthe_num: "自転車盗難件数",
+  Bicthe_rate: "人口に対する自転車盗難件数",
+  Carthe_num: "自動車盗難件数",
+  Carthe_rate: "人口に対する自転車盗難件数",
+  Carac_num: "自動車人身事故件数",
+  Carac_rate: "人口に対する自動車人身事故件数",
+  Snathe_num: "ひったくり件数",
+  Snathe_rate: "人口に対するひったくり件数",
 };
--- database/useCities.ts
+++ database/useCities.ts
@@ -1,11 +1,31 @@
+import { useEffect } from "react";
 import { atom, useRecoilValue, useSetRecoilState } from "recoil";
-import { CityType } from "./dataType";
+import { CityType, importedCitiesType } from "./dataType";
 import { citiesMockData } from "./mockData";
+
+// モックデータやAPIから読み込まれたCityの型をCityTypeに整形する。
+export const formatImportedCities = (cities: importedCitiesType): CityType[] => {
+  return cities.map((city) => ({
+    ...city,
+    Pop: Number.parseInt(city.Pop),
+    Temp_Jan: Number.parseInt(city.Temp_Jan),
+    Temp_July: Number.parseInt(city.Temp_July),
+    Snow_Jan: Number.parseFloat(city.Snow_Jan.split("cm")[0]),
+    Bicthe_num: Number.parseInt(city.Bicthe_num),
+    Bicthe_rate: Number.parseFloat(city.Bicthe_rate.split("%")[0]),
+    Carthe_num: Number.parseInt(city.Carthe_num),
+    Carthe_rate: Number.parseFloat(city.Carthe_rate.split("%")[0]),
+    Carac_num: Number.parseInt(city.Carac_num),
+    Carac_rate: Number.parseFloat(city.Carac_rate.split("%")[0]),
+    Snathe_num: Number.parseInt(city.Snathe_num),
+    Snathe_rate: Number.parseFloat(city.Snathe_rate.split("%")[0]),
+  }));
+};
 
 // 市町村の一覧を獲得する関数。今はモックデータを返すだけ。
 const cityState = atom<CityType[]>({
   key: "cities",
-  default: [],
+  default: formatImportedCities(citiesMockData),
 });
 
 // 市町村の一覧を獲得するhooks。
