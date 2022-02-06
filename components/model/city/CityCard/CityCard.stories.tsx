fdcdd0206988bf48306cfca675f1f3fb18f2ba15
koke-desu <gbpvp25645@yahoo.co.jp>
2022-02-06 15:52:18 +0900

    型エラーの修正

--- components/model/city/CityCard/CityCard.stories.tsx
+++ components/model/city/CityCard/CityCard.stories.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import CityCard from "./CityCard";
 import { citiesMockData } from "../../../../database/mockData";
+import { formatImportedCities } from "../../../../database/useCities";
 
-const mockCity = citiesMockData[0];
+const mockCity = formatImportedCities(citiesMockData);
 
 export default { component: CityCard } as ComponentMeta<typeof CityCard>;
 
-export const Primary: ComponentStory<typeof CityCard> = () => <CityCard city={mockCity} />;
+export const Primary: ComponentStory<typeof CityCard> = () => <CityCard city={mockCity[0]} />;
